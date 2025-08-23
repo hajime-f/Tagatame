ac37d69e84faf73d0c99e9842a5497dc7b913485
Hajime Fujita <hajime.fujita@gmail.com>
2025-08-24 01:06:03 +0900

    キャラクターリストの表示を少し変えた

--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -6,76 +6,88 @@ public class CharacterPointer : MonoBehaviour
     public FixedJoystick fixedJoystick;
     public TextMeshProUGUI pointer;
     public Transform contentParent;
+    public int visibleCount = 5; // 画面に表示可能な行数
 
+    private AudioSource audioSource;
+    public AudioClip cancelSound;
+    public AudioClip selectSound;
+    public AudioClip cursorSound;
+    
     private int selectedIndex = 0;
     private int maxIndex;
+    private int topIndex = 0;
     private bool isStickMoved = false;
+    private float rowHeight = 190;
 
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+	// audioSource.volume = 0.3f;	
+    }    
+    
     public void Initialize(Transform content)
     {
-	contentParent = content;
-	selectedIndex = 0;
+        contentParent = content;
+        selectedIndex = 0;
+        topIndex = 0;
 
-	Canvas.ForceUpdateCanvases();
-	
-	maxIndex = contentParent.childCount - 1;
-	UpdatePointerPosition();
-    }    
+        Canvas.ForceUpdateCanvases();
+
+        maxIndex = contentParent.childCount - 1;
+
+        UpdatePointerAndContent();
+        UpdateVisibleRows();
+    }
 
     void Update()
     {
-        float vertical = this.fixedJoystick.Vertical;
+        float vertical = fixedJoystick.Vertical;
 
-        if (vertical > 0.5f && !this.isStickMoved && selectedIndex > 0)
+        if (vertical > 0.5f && !isStickMoved && selectedIndex > 0)
         {
             selectedIndex--;
-	    this.isStickMoved = true;
-            UpdatePointerPosition();
+            isStickMoved = true;
+            if (selectedIndex < topIndex) topIndex--;
+	    audioSource.PlayOneShot(cursorSound);
+            UpdatePointerAndContent();
+            UpdateVisibleRows();
         }
-        else if (vertical < -0.5f && !this.isStickMoved && selectedIndex < maxIndex)
+        else if (vertical < -0.5f && !isStickMoved && selectedIndex < maxIndex)
         {
             selectedIndex++;
-	    this.isStickMoved = true;
-            UpdatePointerPosition();
+            isStickMoved = true;
+            if (selectedIndex >= topIndex + visibleCount) topIndex++;
+	    audioSource.PlayOneShot(cursorSound);
+            UpdatePointerAndContent();
+            UpdateVisibleRows();
         }
 
-	if (Mathf.Abs(vertical) < 0.2f)
-	{
-	    this.isStickMoved = false;
-	}
+        if (Mathf.Abs(vertical) < 0.2f) isStickMoved = false;
     }
 
-    public int visibleRowCount = 5;   // 画面に表示できる行数
-    private int topIndex = 0;         // 現在表示している最上段の行インデックス
-    
-    void UpdatePointerPosition()
+    void UpdatePointerAndContent()
     {
-	// もし選択行が表示範囲を超えたらスクロール
-	if (selectedIndex < topIndex)
-	{
-	    topIndex = selectedIndex;
-	}
-	else if (selectedIndex > topIndex + visibleRowCount - 1)
-	{
-	    topIndex = selectedIndex - visibleRowCount + 1;
-	}
-	
-	// スクロールの反映: contentParent の位置を調整
-	float rowHeight = 200f; // 行の高さ。環境に合わせて調整
-	Vector3 contentPos = contentParent.localPosition;
-	contentPos.y = topIndex * rowHeight;
-	contentParent.localPosition = contentPos;
-	
-	// ポインタの位置を「表示領域の中の相対インデックス」に合わせる
-	int relativeIndex = selectedIndex - topIndex;
+	int pointerPosInView = selectedIndex - topIndex;
 	RectTransform pointerRect = pointer.rectTransform;
 	
-	Vector3 localPos = pointerRect.localPosition;
-	localPos.y = -relativeIndex * rowHeight - 150;
-	pointerRect.localPosition = localPos;
+	float yOffset = -pointerPosInView * rowHeight + 380;
+	
+	pointerRect.localPosition = new Vector3(
+	    pointerRect.localPosition.x,
+	    yOffset,
+	    pointerRect.localPosition.z
+	);
     }
-    
-    
+
+    void UpdateVisibleRows()
+    {
+        // topIndex から visibleCount までの行だけアクティブ
+        for (int i = 0; i <= maxIndex; i++)
+        {
+            contentParent.GetChild(i).gameObject.SetActive(i >= topIndex && i < topIndex + visibleCount);
+        }
+    }
+
     void SelectCharacter(int index)
     {
         Debug.Log("Selected character index: " + index);
