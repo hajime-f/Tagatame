1eddde406b89a33d78c9ab45086143015960d95e
Hajime Fujita <hajime.fujita@gmail.com>
2025-08-24 16:56:44 +0900

    キャラクターリストの実装を進めた

--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -1,13 +1,34 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class CharacterPointer : MonoBehaviour
-{
+{    
     public FixedJoystick fixedJoystick;
     public TextMeshProUGUI pointer;
     public Transform contentParent;
     public int visibleCount = 5; // 画面に表示可能な行数
 
+    public enum State
+    {
+        ListDisplayed,  // リスト表示中
+        ConfirmReturn,  // 「タイトル画面に戻りますか？」表示中
+	ConfirmRemove,  // 「キャラクターを削除していいですか？」表示中
+	ConfirmProceed, // 「このキャラクターで戦いますか？」表示中
+    }
+    public State state = State.ListDisplayed;
+
+    [TextArea(3, 10)]
+    public string[] texts;
+    
+    private TextMeshProUGUI textMesh;
+    public GameObject messageBox;
+    public GameObject menuSelector;
+    public GameObject navigationMessage;
+    public GameObject aButton;
+    public GameObject bButton;
+    
     private AudioSource audioSource;
     public AudioClip cancelSound;
     public AudioClip selectSound;
@@ -19,8 +40,20 @@ public class CharacterPointer : MonoBehaviour
     private bool isStickMoved = false;
     private float rowHeight = 190;
 
+    public string nextSceneName_01 = "Opening";
+    
     void Start()
     {
+	textMesh = navigationMessage.GetComponent<TextMeshProUGUI>();
+	if (messageBox.activeSelf)
+	    messageBox.SetActive(false);
+	if (menuSelector.activeSelf)
+	    menuSelector.SetActive(false);
+	if (navigationMessage.activeSelf) {
+	    textMesh.text = "";
+	    navigationMessage.SetActive(false);
+	}
+	
         audioSource = gameObject.AddComponent<AudioSource>();
 	// audioSource.volume = 0.3f;	
     }    
@@ -43,26 +76,50 @@ public class CharacterPointer : MonoBehaviour
     {
         float vertical = fixedJoystick.Vertical;
 
-        if (vertical > 0.5f && !isStickMoved && selectedIndex > 0)
+	if (state == State.ListDisplayed) {
+	    if (vertical > 0.5f && !isStickMoved && selectedIndex > 0)
+	    {
+		selectedIndex--;
+		isStickMoved = true;
+		if (selectedIndex < topIndex) topIndex--;
+		audioSource.PlayOneShot(cursorSound);
+		UpdatePointerAndContent();
+		UpdateVisibleRows();
+	    }
+	    else if (vertical < -0.5f && !isStickMoved && selectedIndex < maxIndex)
+	    {
+		selectedIndex++;
+		isStickMoved = true;
+		if (selectedIndex >= topIndex + visibleCount) topIndex++;
+		audioSource.PlayOneShot(cursorSound);
+		UpdatePointerAndContent();
+		UpdateVisibleRows();
+	    }
+	    
+	    if (Mathf.Abs(vertical) < 0.2f) isStickMoved = false;
+	}
+	
+	// Aボタンの押下を検知
+	var aScript = aButton.GetComponent<OnMouseDownShow_A>();
+	bool a_isPressed = aScript.isPressed;
+	
+        if (a_isPressed)
         {
-            selectedIndex--;
-            isStickMoved = true;
-            if (selectedIndex < topIndex) topIndex--;
-	    audioSource.PlayOneShot(cursorSound);
-            UpdatePointerAndContent();
-            UpdateVisibleRows();
-        }
-        else if (vertical < -0.5f && !isStickMoved && selectedIndex < maxIndex)
-        {
-            selectedIndex++;
-            isStickMoved = true;
-            if (selectedIndex >= topIndex + visibleCount) topIndex++;
-	    audioSource.PlayOneShot(cursorSound);
-            UpdatePointerAndContent();
-            UpdateVisibleRows();
+            OnAButtonPressed();
+	    if (a_isPressed)
+		aScript.isPressed = false;
         }
 
-        if (Mathf.Abs(vertical) < 0.2f) isStickMoved = false;
+	// Bボタンの押下を検知
+	var bScript = bButton.GetComponent<OnMouseDownShow_B>();
+	bool b_isPressed = bScript.isPressed;
+	
+        if (b_isPressed)
+        {
+            OnBButtonPressed();
+	    if (b_isPressed)
+		bScript.isPressed = false;
+        }        	
     }
 
     void UpdatePointerAndContent()
@@ -88,9 +145,82 @@ public class CharacterPointer : MonoBehaviour
         }
     }
 
-    void SelectCharacter(int index)
+    public void OnAButtonPressed()
     {
-        Debug.Log("Selected character index: " + index);
-        // 必要ならここでデータを取得
+	var selector = menuSelector.GetComponent<MenuSelector_3>();
+	int selectedMenu = selector.selectedMenu;
+
+	switch (state)
+	{
+	    case State.ListDisplayed:
+		break;
+	    
+	    case State.ConfirmReturn:
+		if (selectedMenu == 0)
+		{
+		    HideUI();
+		    state = State.ListDisplayed;
+		    if (cancelSound != null)
+			audioSource.PlayOneShot(cancelSound);
+		}
+		else
+		{
+		    if (selectSound != null)
+			StartCoroutine(PlaySoundAndLoadScene(selectSound, nextSceneName_01));
+		}
+		break;
+	}
+	
     }
+
+    public void OnBButtonPressed()
+    {
+	switch (state)
+	{
+	    case State.ListDisplayed:
+		ShowUI(texts[0]);
+		state = State.ConfirmReturn;
+		if (cancelSound != null)
+		    audioSource.PlayOneShot(cancelSound);
+		break;
+
+	    case State.ConfirmReturn:
+		HideUI();
+		state = State.ListDisplayed;
+		if (cancelSound != null)
+		    audioSource.PlayOneShot(cancelSound);
+		break;
+	}
+    }
+
+    private void HideUI()
+    {
+	if (messageBox.activeSelf)
+	    messageBox.SetActive(false);
+	if (menuSelector.activeSelf)
+	    menuSelector.SetActive(false);
+	if (navigationMessage.activeSelf) {
+	    textMesh.text = "";
+	    navigationMessage.SetActive(false);
+	}
+    }    
+
+    private void ShowUI(string text)
+    {
+	if (!messageBox.activeSelf)
+	    messageBox.SetActive(true);
+	if (!menuSelector.activeSelf)
+	    menuSelector.SetActive(true);
+	if (!navigationMessage.activeSelf) {
+	    textMesh.text = text;
+	    navigationMessage.SetActive(true);
+	}
+    }            
+
+    private IEnumerator PlaySoundAndLoadScene(AudioClip clip, string sceneName)
+    {
+	audioSource.PlayOneShot(clip);
+	yield return new WaitForSeconds(clip.length); // 音が鳴り終わるまで待機
+	SceneManager.LoadScene(sceneName);
+    }        
 }
---
+++ Assets/Scripts/MenuSelector_3.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MenuSelector_3 : MonoBehaviour
+{
+    public FixedJoystick fixedJoystick;
+    public int selectedMenu = 0;
+    private bool isStickMoved = false;
+    private AudioSource audioSource;
+    public GameObject menuNav;
+    public int maxMenu = 1;
+
+    void Start()
+    {
+	Application.targetFrameRate = 60;
+	audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+	float verticalInput = this.fixedJoystick.Vertical;
+	
+	CharacterPointer.State state = menuNav.GetComponent<CharacterPointer>().state;
+
+	if (state != CharacterPointer.State.ListDisplayed)
+	{
+	    if (verticalInput < -0.5f && !this.isStickMoved && selectedMenu != maxMenu)
+	    {
+		this.selectedMenu++;
+		transform.Translate(-86, 0, 0);
+		this.isStickMoved = true;
+		audioSource.Play();
+	    } else if (verticalInput > 0.5f && !this.isStickMoved && selectedMenu != 0)
+	    {
+		this.selectedMenu--;
+		transform.Translate(86, 0, 0);
+		this.isStickMoved = true;
+		audioSource.Play();
+	    }
+	    
+	    if (Mathf.Abs(verticalInput) < 0.2f)
+	    {
+		this.isStickMoved = false;
+	    }	            
+	}        
+    }
+}
